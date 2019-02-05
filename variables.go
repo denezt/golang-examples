b7841a494ef313436b7e0611bbd0e5c6e993f898
Richard L. Jackson <denezt@users.noreply.github.com>
2019-02-05 22:28:31 +0100

    Create variables.go

---
+++ variables.go
@@ -0,0 +1,12 @@
+package main
+import "fmt"
+func main () {
+  var x int = 5 //declaration of variable x
+  var (
+  a = 10
+  b = 15 //declaration of multiple variables
+  )
+  y := 7 // shorthand declaration of variables
+  const pi float64 = 3.14272345 //declaration of constants
+  var name string = "Will Smiths" //declaration of a string
+}

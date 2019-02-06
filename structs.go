66eb71e226e9531ecdc8d111d013ed7a86ab3d21
Richard L. Jackson <denezt@users.noreply.github.com>
2019-02-06 20:55:40 +0100

    Create structs.go

---
+++ structs.go
@@ -0,0 +1,15 @@
+package main
+import "fmt"
+// STRUCTS
+func main() {
+rect1 := Rectangle{height: 10, width: 10}
+fmt.Println("Rectangle is", rect1.width, "wide")
+fmt.Println("Area of the rectangle =", rect1.area())
+}
+type Rectangle struct{
+height float64
+width float64
+}
+func (rect *Rectangle) area() float64{
+return rect.width * rect.height
+}

07f5f4ebd709d9f9d064b5a263356f3eb8d10a2f
Richard L. Jackson <denezt@users.noreply.github.com>
2019-02-06 20:43:22 +0100

    Create looping_logic.go

---
+++ looping_logic.go
@@ -0,0 +1,16 @@
+package main
+import "fmt"
+func main() {
+// For loops
+i := 1
+for i <= 10 {
+fmt.Println(i)
+// Shorthand for i = i + 1
+i++
+}
+// Relational Operators include ==, !=, <, >, <=, and >=
+// You can also define a for loop like this, but you need semicolons
+for j := 0; j < 5; j++ {
+fmt.Println(j);
+}
+}

50ec8d04148cdffc0f40901e2b3608bf80902b06
Richard L. Jackson <denezt@users.noreply.github.com>
2019-02-06 20:48:18 +0100

    Create working_with_functions.go

---
+++ working_with_functions.go
@@ -0,0 +1,16 @@
+package main
+import "fmt"
+func main () {
+fmt.Println("5 + 4 = ", add(5,4))
+fmt.Println(subtract(1,2,3,4,5))
+}
+func add(a,b int) int {
+return a+b
+}
+func subtract(args ... int) {
+sub := 0
+for _, value := range args {
+sub -= value
+}
+return sub
+}
